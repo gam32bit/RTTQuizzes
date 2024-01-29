886980f9376aaec9a7fdad3e5d6857c2fc62c2db
gam32bit <joseph.caterine@gmail.com>
2024-01-29 11:56:07 +0000

    stop visual feedback after selection

--- scripts/main.js
+++ scripts/main.js
@@ -18,26 +18,32 @@ document.addEventListener('DOMContentLoaded', function () {
                 quizContainer.appendChild(questionElement);
 
                 const options = questionElement.querySelectorAll('li');
+                let answerSelected = false;
 
                 options.forEach(option => {
-                    option.addEventListener('click', () => handleOptionClick(option, question.correctOption, question.context));
+                    option.addEventListener('click', function () {
+                        if (!answerSelected) {
+                            answerSelected = true;
+                            handleOptionClick(this, question.correctOption, question.context, options);
+                        }
+                    });
                 });
             });
         })
         .catch(error => console.error('Error fetching JSON:', error));
 
-    function handleOptionClick(selectedOption, correctOption, context) {
+    function handleOptionClick(selectedOption, correctOption, context, options) {
         const selectedOptionIndex = parseInt(selectedOption.getAttribute('data-index'), 10);
         const additionalContextElement = document.querySelector('.additionalcontext');
 
         if (selectedOptionIndex === correctOption) {
             // Correct option chosen
             selectedOption.style.color = 'green';
-            
+            additionalContextElement.textContent = context;
         } else {
             // Incorrect option chosen
             selectedOption.style.color = 'red';
-            
+
             // Highlight the correct option in green
             const correctOptionElement = selectedOption.parentNode.querySelector(`li[data-index="${correctOption}"]`);
             correctOptionElement.style.color = 'green';
@@ -45,5 +51,4 @@ document.addEventListener('DOMContentLoaded', function () {
             additionalContextElement.textContent = context;
         }
     }
-
 });
