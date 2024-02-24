66ffb2c4562acf37fd2ba555bf3eb26c448d7d8b
gam32bit <joseph.caterine@gmail.com>
2024-02-24 14:29:51 +0000

    loading problem

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function () {
     let correctAnswersCount = 0;
     let answeredQuestionsCount = 0;
     let quizContainer;
@@ -6,9 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     async function fetchQuizList() {
         try {
-            const response = await fetch('/quizzes/quiz_list.json'); // Path change for absolute referencing
-            const quizList = await response.json();
-            return quizList;
+            const response = await fetch('/quizzes/quiz_list.json');
+            return await response.json();
         } catch (error) {
             console.error('Error fetching quiz list:', error);
         }
@@ -19,158 +18,159 @@ document.addEventListener('DOMContentLoaded', function () {
         quizGallery.innerHTML = ''; // Clear existing quiz cards
 
         quizList.forEach(quiz => {
-            const quizCard = document.createElement('div');
-            quizCard.classList.add('quiz-card');
-            quizCard.innerHTML = ` 
-                <img src="/img/${quiz.image}" alt="${quiz.title} Quiz"> 
-                <div class="quiz-info">
-                    <h3>${quiz.title}</h3> 
-                    <button class="take-quiz-btn" data-quizfile="${quiz.filename}">Take Quiz</button> 
-                </div>
-            `;
+            const quizCard = createQuizCardElement(quiz);
             quizGallery.appendChild(quizCard);
         });
     }
 
+    function createQuizCardElement(quiz) {
+        const quizCard = document.createElement('div');
+        quizCard.classList.add('quiz-card');
+        quizCard.innerHTML = ` 
+            <img src="/img/${quiz.image}" alt="${quiz.title} Quiz"> 
+            <div class="quiz-info">
+                <h3>${quiz.title}</h3> 
+                <button class="take-quiz-btn" data-quizfile="${quiz.filename}">Take Quiz</button> 
+            </div>
+        `;
+        return quizCard;
+    }
+
     async function initializeQuiz(quizFilename) {
         try {
-            const response = await fetch(`/quizzes/${quizFilename}`); // Path change for absolute referencing
-            if (!response.ok) { 
+            const response = await fetch(`/quizzes/${quizFilename}`);
+            if (!response.ok) {
                 throw new Error(`Error fetching quiz data: ${response.status}`);
             }
-            const quizData = await response.json();
-            initializeQuizUI(quizData); 
+            quizData = await response.json();
+            initializeQuizUI();
         } catch (error) {
             console.error('Error loading quiz:', error);
             displayErrorMessage('There was a problem loading the quiz. Please try again later.');
         }
     }
 
-    function initializeQuizUI(quizData) { // Changed to receive and use quizData
+    function initializeQuizUI() {
         document.querySelector('.quiz-title').textContent = quizData.month;
         quizContainer = document.querySelector('.quiz-container');
 
         quizData.questions.forEach((question, index) => {
             const questionElement = createQuestionElement(question, index);
             const additionalContextElement = createAdditionalContextElement();
-
+            addOptionClickHandlers(question, questionElement, additionalContextElement);
             quizContainer.appendChild(questionElement);
             quizContainer.appendChild(additionalContextElement);
+        });
+    }
 
-            const options = questionElement.querySelectorAll('li');
+    function createQuestionElement(question, index) {
+        const questionElement = document.createElement('div');
+        questionElement.classList.add('question');
+        questionElement.innerHTML = `
+            <p>${index + 1}. ${question.question}</p>
+            <ul>
+                ${question.options.map((option, i) => `<li data-index="${i}">${String.fromCharCode(65 + i)}. ${option}</li>`).join('')}
+            </ul>
+        `;
+        return questionElement;
+    }
 
-            options.forEach(option => {
-                option.addEventListener('click', function () {
-                    handleOptionClick(this, question.correctOption, question.context, additionalContextElement, options, questionElement);
-                });
+    function createAdditionalContextElement() {
+        const additionalContextElement = document.createElement('div');
+        additionalContextElement.classList.add('additionalcontext');
+        return additionalContextElement;
+    }
+
+    function addOptionClickHandlers(question, questionElement, additionalContextElement) {
+        const options = questionElement.querySelectorAll('li');
+
+        options.forEach(option => {
+            option.addEventListener('click', function () {
+                handleOptionClick(this, question.correctOption, question.context, additionalContextElement, options, questionElement);
             });
         });
     }
 
-        function createQuestionElement(question, index) {
-            const questionElement = document.createElement('div');
-            questionElement.classList.add('question');
-            questionElement.innerHTML = `
-                <p>${index + 1}. ${question.question}</p>
-                <ul>
-                    ${question.options.map((option, i) => `<li data-index="${i}">${String.fromCharCode(65 + i)}. ${option}</li>`).join('')}
-                </ul>
-            `;
-            return questionElement;
+    function handleOptionClick(selectedOption, correctOption, context, additionalContextElement, options, questionElement) {
+        if (questionElement.classList.contains('answered')) {
+            return;
         }
 
-        function createAdditionalContextElement() {
-            const additionalContextElement = document.createElement('div');
-            additionalContextElement.classList.add('additionalcontext');
-            return additionalContextElement;
+        const selectedOptionIndex = parseInt(selectedOption.getAttribute('data-index'), 10);
+
+        if (selectedOptionIndex === correctOption) {
+            selectedOption.style.color = 'green';
+            additionalContextElement.textContent = context;
+            correctAnswersCount++;
+        } else {
+            selectedOption.style.color = 'red';
+            const correctOptionElement = selectedOption.parentNode.querySelector(`li[data-index="${correctOption}"]`);
+            correctOptionElement.style.color = 'green';
+            additionalContextElement.textContent = context;
         }
 
-        function handleOptionClick(selectedOption, correctOption, context, additionalContextElement, options, questionElement) {
-            // Check if the question has already been answered
-            if (questionElement.classList.contains('answered')) {
-                return;
-            }
+        answeredQuestionsCount++;
+        questionElement.classList.add('answered');
 
-            const selectedOptionIndex = parseInt(selectedOption.getAttribute('data-index'), 10);
+        if (answeredQuestionsCount === quizData.questions.length) {
+            displayResultMessage(correctAnswersCount, quizData.questions.length);
+        }
+    }
 
-            if (selectedOptionIndex === correctOption) {
-                // Correct option chosen
-                selectedOption.style.color = 'green';
-                additionalContextElement.textContent = context;
-                correctAnswersCount++;
-            } else {
-                // Incorrect option chosen
-                selectedOption.style.color = 'red';
-
-                // Highlight the correct option in green
-                const correctOptionElement = selectedOption.parentNode.querySelector(`li[data-index="${correctOption}"]`);
-                correctOptionElement.style.color = 'green';
-
-                additionalContextElement.textContent = context;
-            }
-
-            answeredQuestionsCount++;
-
-            // Mark the question as answered to prevent further clicks
-            questionElement.classList.add('answered');
-
-            if (answeredQuestionsCount === quizData.questions.length) {
-                displayResultMessage(correctAnswersCount, quizData.questions.length);
-            }
+    function resetQuiz() {
+        const resultMessage = quizContainer.querySelector('.result-message');
+        if (resultMessage) {
+            quizContainer.removeChild(resultMessage);
         }
 
-        function resetQuiz() {
-            // Reset the quiz by removing the result message, resetting counters, and clearing styles
-            const resultMessage = quizContainer.querySelector('.result-message');
-            if (resultMessage) {
-                quizContainer.removeChild(resultMessage);
-            }
+        correctAnswersCount = 0;
+        answeredQuestionsCount = 0;
 
-            correctAnswersCount = 0;
-            answeredQuestionsCount = 0;
+        const questions = quizContainer.querySelectorAll('.question');
+        questions.forEach(question => {
+            question.classList.remove('answered');
+        });
 
-            // Remove the 'answered' class for each question
-            const questions = quizContainer.querySelectorAll('.question');
-            questions.forEach(question => {
-                question.classList.remove('answered');
-            });
+        const options = quizContainer.querySelectorAll('.question li');
+        options.forEach(option => {
+            option.style.color = '';
+        });
 
-            const options = quizContainer.querySelectorAll('.question li');
-            options.forEach(option => {
-                option.style.color = ''; // Reset text color
-            });
+        const additionalContextElements = quizContainer.querySelectorAll('.additionalcontext');
+        additionalContextElements.forEach(element => {
+            element.textContent = '';
+        });
+    }
 
-            // Clear additional context for each question
-            const additionalContextElements = quizContainer.querySelectorAll('.additionalcontext');
-            additionalContextElements.forEach(element => {
-                element.textContent = '';
-            });
-        }
+    function displayResultMessage(correctCount, totalCount) {
+        const resultMessage = document.createElement('div');
+        resultMessage.classList.add('result-message');
+        resultMessage.textContent = `You got ${correctCount} out of ${totalCount} correct!`;
 
-        function displayResultMessage(correctCount, totalCount) {
-            const resultMessage = document.createElement('div');
-            resultMessage.classList.add('result-message');
-            resultMessage.textContent = `You got ${correctCount} out of ${totalCount} correct!`;
+        const resetButton = document.createElement('button');
+        resetButton.textContent = 'Reset Quiz';
+        resetButton.addEventListener('click', resetQuiz);
+        resultMessage.appendChild(resetButton);
 
-            // Create and append the reset button
-            const resetButton = document.createElement('button');
-            resetButton.textContent = 'Reset Quiz';
-            resetButton.addEventListener('click', resetQuiz);
-            resultMessage.appendChild(resetButton);
+        quizContainer.appendChild(resultMessage);
+    }
 
-            quizContainer.appendChild(resultMessage);
-        }
+    // Logic to handle quiz selection
+    const urlParams = new URLSearchParams(window.location.search);
+    const selectedQuiz = urlParams.get('quiz');
 
-    document.addEventListener('DOMContentLoaded', async function () {
+    if (selectedQuiz) {
+        await initializeQuiz(selectedQuiz);
+    } else {
         const quizList = await fetchQuizList();
         updateQuizGallery(quizList);
+    }
 
-        // Event delegation for handling quiz selection
-        document.querySelector('.quiz-gallery').addEventListener('click', (event) => {
-            if (event.target.classList.contains('take-quiz-btn')) {
-                const quizFilename = event.target.getAttribute('data-quizfile');
-                window.location.href = 'quiz.html?quiz=' + quizFilename; 
-            }
-            });
-        });
+    // Event delegation for handling quiz selection
+    document.querySelector('.quiz-gallery').addEventListener('click', (event) => {
+        if (event.target.classList.contains('take-quiz-btn')) {
+            const quizFilename = event.target.getAttribute('data-quizfile');
+            window.location.href = 'quiz.html?quiz=' + quizFilename;
+        }
     });
+});
